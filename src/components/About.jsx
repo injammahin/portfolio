9e3b7cfebcd2137e4d1f40feaf2a6667bc645bf2
injammahin <injammahin5507@gmail.com>
2023-12-01 03:05:44 +0600

    a

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,14 +6,14 @@ const About = () => {
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
-            <p className=" text-4xl font-bold inline border-b-4 border-pink-800 ">
+            <p className=" text-4xl font-bold inline border-b-4 border-pink-800  hover:rotate-45%">
               about
             </p>
           </div>
           <div></div>
         </div>
         <div className="max-2-[1000px] w-full grid sm:grid-cols-2 gap-8 xl-4">
-          <div className="sm:text-right text-4xl font-bold">
+          <div className="sm:text-right text-4xl font-bold ">
             <p> hello, I am mahin,nice to met you .Please take a look around</p>
           </div>
           <div>
